82324437398efc93c9feedbb895f9687e8fcc4ee
miyamoto <area.kantou8008@icloud.com>
2020-07-31 23:01:30 +0900

    callbacks useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 const App = props => {
   const [state, setState] = useState(props)
   const {name, price} = state
+
+  useEffect(() => {
+    console.log('hoge')
+  })
+
+  useEffect(() => {
+    console.log('hoge2222222')
+  }, [])
+
+  useEffect(() => {
+    console.log('nameが呼ばれたら')
+  }, [name])
 
   return (
     <>
