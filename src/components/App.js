4e61632e9c50040089ae860321d0afc7db36f3e7
miyamoto <area.kantou8008@icloud.com>
2020-08-05 22:19:24 +0900

    implement event creation

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,44 @@
-import React from 'react';
+import React, { useState, useReducer } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import reducer from '../reducers';
 
 const App = () => {
+  // stateで状態を管理
+  const [state, dispatch] = useReducer(reducer, [])
+  // title, bodyは初期状態は空
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+
+  // イベント作成がクリックされたら
+  const addEvent = e => {
+    // 画面のリロードをなくす
+    e.preventDefault()
+    // reducerのdispatchをここで呼べる
+    dispatch({
+      type: 'CREATE_EVENT',
+      title,
+      body
+    })
+
+    // input内の初期valueをここで空にする
+    // イベント作成後、フォームの中身が空になる 
+    setTitle('')
+    setBody('')
+  }
+
   return (
     <div className="container-fluid">
       <h4>イベント作成フォーム</h4>
       <form>
         <div className="form-group">
           <label htmlFor="title">タイトル</label>
-          <input type="text" className="form-control" id="title" />
+          <input type="text" className="form-control" id="title" value={title} onChange={e => setTitle(e.target.value)} />
         </div>
         <div className="form-group">
           <label htmlFor="body">ボディ</label>
-          <textarea type="text" className="form-control" id="body" />
+          <textarea type="text" className="form-control" id="body" value={body} onChange={e => setBody(e.target.value)} />
         </div>
-        <button className="btn btn-primary">イベントを作成する</button>
+        <button className="btn btn-primary" onClick={addEvent}>イベントを作成する</button>
         <button className="btn btn-danger">全てのイベントを削除する</button>
       </form>
       <h4>イベント一覧</h4>
