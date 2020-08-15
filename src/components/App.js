0b02cab5ac37718ed94fb3b194b7597ebf503017
miyamoto <area.kantou8008@icloud.com>
2020-08-15 20:24:48 +0900

    refactor components

--- src/components/App.js
+++ src/components/App.js
@@ -1,70 +1,17 @@
-import React, { useState, useReducer } from 'react';
+import React, { useReducer } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reducer from '../reducers';
-import Event from './Event'
+import Events from './Events'
+import EventForm from './EventForm'
 
 const App = () => {
   // stateで状態を管理
   const [state, dispatch] = useReducer(reducer, [])
-  // title, bodyは初期状態は空
-  const [title, setTitle] = useState('')
-  const [body, setBody] = useState('')
-
-  // イベント作成がクリックされたら
-  const addEvent = e => {
-    // 画面のリロードをなくす
-    e.preventDefault()
-    // reducerのdispatchをここで呼べる
-    dispatch({
-      type: 'CREATE_EVENT',
-      title,
-      body
-    })
-
-    // input内の初期valueをここで空にする
-    // イベント作成後、フォームの中身が空になる 
-    setTitle('')
-    setBody('')
-  }
-
-  const deleteAllEvents = e => {
-    e.preventDefault()
-    const result = window.confirm('全てのイベントを本当に削除しても良いですか？')
-    if (result) dispatch({ type: 'DELETE_ALL_EVENTS' })
-  }
-
-  const unCreatable = title === '' || body === ''
-
 
   return (
     <div className="container-fluid">
-      <h4>イベント作成フォーム</h4>
-      <form>
-        <div className="form-group">
-          <label htmlFor="title">タイトル</label>
-          <input type="text" className="form-control" id="title" value={title} onChange={e => setTitle(e.target.value)} />
-        </div>
-        <div className="form-group">
-          <label htmlFor="body">ボディ</label>
-          <textarea type="text" className="form-control" id="body" value={body} onChange={e => setBody(e.target.value)} />
-        </div>
-        <button className="btn btn-primary" onClick={addEvent} disabled={unCreatable}>イベントを作成する</button>
-        <button className="btn btn-danger" onClick={deleteAllEvents} disabled={state.length === 0}>全てのイベントを削除する</button>
-      </form>
-      <h4>イベント一覧</h4>
-      <table className="table table-hover">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>タイトル</th>
-            <th>ボディ</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          { state.map((event, index) => (<Event key={index} event={event} dispatch={dispatch} />)) }
-        </tbody>
-      </table>
+      <EventForm state={state} dispatch={dispatch} />
+      <Events state={state} dispatch={dispatch} />
     </div>
   )
 }
---
+++ src/components/EventForm.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+
+const EventForm = ({ state, dispatch }) => {
+  // title, bodyは初期状態は空
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+
+  // イベント作成がクリックされたら
+  const addEvent = e => {
+    // 画面のリロードをなくす
+    e.preventDefault()
+    // reducerのdispatchをここで呼べる
+    dispatch({
+      type: 'CREATE_EVENT',
+      title,
+      body
+    })
+
+    // input内の初期valueをここで空にする
+    // イベント作成後、フォームの中身が空になる 
+    setTitle('')
+    setBody('')
+  }
+
+  const deleteAllEvents = e => {
+    e.preventDefault()
+    const result = window.confirm('全てのイベントを本当に削除しても良いですか？')
+    if (result) dispatch({ type: 'DELETE_ALL_EVENTS' })
+  }
+
+  const unCreatable = title === '' || body === ''
+
+  return (
+    <>
+      <h4>イベント作成フォーム</h4>
+      <form>
+        <div className="form-group">
+          <label htmlFor="title">タイトル</label>
+          <input type="text" className="form-control" id="title" value={title} onChange={e => setTitle(e.target.value)} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="body">ボディ</label>
+          <textarea type="text" className="form-control" id="body" value={body} onChange={e => setBody(e.target.value)} />
+        </div>
+        <button className="btn btn-primary" onClick={addEvent} disabled={unCreatable}>イベントを作成する</button>
+        <button className="btn btn-danger" onClick={deleteAllEvents} disabled={state.length === 0}>全てのイベントを削除する</button>
+      </form>
+    </>
+  )
+}
+
+export default EventForm
