0ab828cbc2b04da186fa5e02190917389b8a7c20
miyamoto <area.kantou8008@icloud.com>
2020-08-16 13:45:33 +0900

    replace type names with constants

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { CREATE_EVENT, DELETE_ALL_EVENTS } from '../actions'
 
 const EventForm = ({ state, dispatch }) => {
   // title, bodyは初期状態は空
@@ -11,7 +12,7 @@ const EventForm = ({ state, dispatch }) => {
     e.preventDefault()
     // reducerのdispatchをここで呼べる
     dispatch({
-      type: 'CREATE_EVENT',
+      type: CREATE_EVENT,
       title,
       body
     })
@@ -25,7 +26,7 @@ const EventForm = ({ state, dispatch }) => {
   const deleteAllEvents = e => {
     e.preventDefault()
     const result = window.confirm('全てのイベントを本当に削除しても良いですか？')
-    if (result) dispatch({ type: 'DELETE_ALL_EVENTS' })
+    if (result) dispatch({ type: DELETE_ALL_EVENTS })
   }
 
   const unCreatable = title === '' || body === ''
