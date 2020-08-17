5229fe6b8875681056b918979a87b481c42f9d3d
miyamoto <area.kantou8008@icloud.com>
2020-08-17 14:08:38 +0900

    -a

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
 import React, { useReducer } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import reducer from '../reducers';
+import reducer from '../reducers/events';
 import Events from './Events'
 import EventForm from './EventForm'
 import AppContext from '../contexts/AppContext'
 
 const App = () => {
+  const initialState = { events: [] }
   // stateで状態を管理
-  const [state, dispatch] = useReducer(reducer, [])
-
+  const [state, dispatch] = useReducer(reducer, initialState)
   return (
     <AppContext.Provider value={{ state, dispatch }}>
       <div className="container-fluid">
