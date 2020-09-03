0fbbbd7a2bc28148b085bff736578d80a47747cf
miyamoto <area.kantou8008@icloud.com>
2020-09-03 22:22:57 +0900

    display all operationLogs

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import reducer from '../reducers'
 import Events from './Events'
 import EventForm from './EventForm'
+import OperationLogs from './OperationLogs'
 import AppContext from '../contexts/AppContext'
 
 const App = () => {
@@ -18,6 +19,7 @@ const App = () => {
       <div className="container-fluid">
         <EventForm />
         <Events />
+        <OperationLogs />
       </div>
     </AppContext.Provider>
   )
